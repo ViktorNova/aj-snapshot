e5378317125f62fd97b7005c66f9b5b8a7eca9f5
lieven moors <lieven@laptop.(none)>
2010-03-05 00:11:50 +0100

    second commit

--- aj-alsa.c
+++ aj-alsa.c
@@ -4,37 +4,35 @@ const char* xml_whitespace_cb( mxml_node_t *node, int where)
 {
 	const char *name;
 	name = node->value.element.name;
-	if ( strcmp(name, "alsa") || strcmp(name, "jack"))
+	if ( !strcmp(name, "alsa") || !strcmp(name, "jack"))
 	{
-		if (where == MXML_WS_BEFORE_OPEN || where == MXML_WS_AFTER_CLOSE)
+		if (where == MXML_WS_BEFORE_OPEN || where == MXML_WS_BEFORE_CLOSE)
 		return ("\n");
-
+	}
+	if ( !strcmp(name, "client") )
+	{
+		if ( where == MXML_WS_BEFORE_OPEN )
+		return ("\n  ");
 	}
 	return NULL;
 }
 
 void alsa_store_connected_clients( snd_seq_t* seq, mxml_node_t* alsa_node )
 {
-
-	int id;
 	const char* name;
 
 	snd_seq_client_info_t* cinfo;
 	snd_seq_client_info_alloca(&cinfo);
 	snd_seq_client_info_set_client(cinfo, -1);
 
-	//printf("id = %d", snd_seq_client_id(seq));
-
 	while (snd_seq_query_next_client(seq, cinfo) >= 0) 
 	{
-		id = snd_seq_client_info_get_client(cinfo);
 		name = snd_seq_client_info_get_name(cinfo);
 
 		mxml_node_t* client_node;
-		client_node = mxmlNewElement(alsa_node, name);
-		mxmlNewInteger(client_node, id);
+		client_node = mxmlNewElement(alsa_node, "client");
+		mxmlElementSetAttr(client_node, "name", name);
 	}
-
 }
 
 void alsa_store( snd_seq_t* seq, const char* filename )
@@ -54,7 +52,6 @@ void alsa_store( snd_seq_t* seq, const char* filename )
 	}
 	mxmlSaveFile(xml_node, fp, xml_whitespace_cb);
 	fclose(fp);
-
 }
 
 snd_seq_t* alsa_initialize( snd_seq_t* seq )
