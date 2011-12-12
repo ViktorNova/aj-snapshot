97e2be0286407316aaac3bf6650cb62a1758740a
lieven moors <lievenmoors@gmail.com>
2011-12-12 22:30:36 +0100

    free more allocated stuff in alsa_store_connections

--- src/aj-alsa.c
+++ src/aj-alsa.c
@@ -68,6 +68,9 @@ void alsa_store_connections( snd_seq_t* seq, const snd_seq_addr_t *addr, mxml_no
 		mxmlElementSetAttr(connection_node, "client", client_name);
 		mxmlElementSetAttr(connection_node, "port", port_id);
 	}		
+	snd_seq_query_subscribe_free(subs);
+	snd_seq_client_info_free(connected_cinfo);
+	snd_seq_port_info_free(connected_pinfo);
 }
 
 void alsa_store_ports( snd_seq_t* seq, snd_seq_client_info_t* cinfo, snd_seq_port_info_t* pinfo, mxml_node_t* client_node )
